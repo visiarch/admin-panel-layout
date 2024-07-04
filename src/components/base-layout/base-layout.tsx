a6efafbf1b9d1db3fcc8a3ecdab0d3264cf3f639
Gusti Bagus Suandana <gusti@vinceredigital.com>
2024-07-04 16:46:27 +0800

    context

--- src/components/base-layout/base-layout.tsx
+++ src/components/base-layout/base-layout.tsx
@@ -1,96 +1,67 @@
 import React, { PropsWithChildren, useState } from "react";
-import Navbar from "../navbar";
-
-/**
- * The Props type for the BaseLayout component.
- *
- * @typedef {Object} Props
- * @property {React.ReactNode} [topbarEnd] - The content that appears at the end of the top bar.
- * @property {React.ReactNode} [sideMenu] - The content that appears in the sidebar.
- * @property {React.ReactNode} [brand] - The brand content that appears in the top bar.
- * @property {React.ReactNode} [closeIcon] - The icon that appears when the sidebar is open and is used to close it.
- * @property {React.ReactNode} [openIcon] - The icon that appears when the sidebar is closed and is used to open it.
- */
+import { PanelLeftClose, PanelLeftOpen } from "lucide-react";
+import { BaseLayoutProvider, useBaseLayout } from "./base-layout-context";
 
 type Props = {
-    /**
-     * The content that appears at the end of the top bar.
-     */
     topbarEnd?: React.ReactNode;
-    /**
-     * The content that appears in the sidebar.
-     */
     sideMenu?: React.ReactNode;
-    /**
-     * The brand content that appears in the top bar.
-     */
     brand?: React.ReactNode;
-    /**
-     * The icon that appears when the sidebar is open and is used to close it.
-     */
+    iconBrand?: React.ReactNode;
     closeIcon?: React.ReactNode;
-    /**
-     * The icon that appears when the sidebar is closed and is used to open it.
-     */
     openIcon?: React.ReactNode;
 };
-/**
- * The BaseLayout component renders a layout with a sidebar and a main content area.
- * It takes in various props to customize the appearance of the layout.
- *
- * @param {Props} props - The props object containing the customization options.
- * @param {React.ReactNode} [props.sideMenu] - The content to be displayed in the sidebar.
- * @param {React.ReactNode} [props.brand] - The content to be displayed in the top bar.
- * @param {React.ReactNode} [props.children] - The main content to be displayed in the main content area.
- * @param {React.ReactNode} [props.closeIcon] - The icon to be displayed when the sidebar is open and used to close it.
- * @param {React.ReactNode} [props.openIcon] - The icon to be displayed when the sidebar is closed and used to open it.
- * @param {React.ReactNode} [props.topbarEnd] - The content to be displayed at the end of the top bar.
- * @returns {JSX.Element} - The rendered layout component.
- */
+
 const BaseLayout = ({
     sideMenu,
     brand,
+    iconBrand,
     children,
     closeIcon,
     openIcon,
     topbarEnd,
 }: PropsWithChildren<Props>) => {
-    // State to track the open/close state of the sidebar
     const [isOpen, setIsOpen] = useState(false);
+    const { isCollapsed, setIsCollapsed } = useBaseLayout();
 
     return (
         <section
-            className="min-h-screen antialiased font-normal fi-body fi-panel-admin bg-gray-50 text-gray-950 dark:bg-gray-950 dark:text-white"
+            className="min-h-screen antialiased font-normal bg-gray-50 text-gray-950 dark:bg-gray-950 dark:text-white"
             style={{ overflow: "visible" }}
         >
-            <div className="flex flex-row-reverse w-full min-h-screen fi-layout overflow-x-clip">
-                {/* Main content area */}
+            <div className="flex flex-row-reverse w-full min-h-screen overflow-x-clip">
                 <div
                     style={{ display: "flex", opacity: 1 }}
-                    className="flex-col flex-1 w-screen opacity-0 fi-main-ctn"
+                    className="flex-col flex-1 w-screen opacity-0 "
                 >
-                    {/* Top bar */}
-                    <div className="sticky top-0 z-20 fi-topbar overflow-x-clip">
-                        {/* Navbar component */}
-                        <Navbar
-                            closeIcon={closeIcon}
-                            openIcon={openIcon}
-                            isOpen={isOpen}
-                            setIsOpen={setIsOpen}
-                            topbarEnd={topbarEnd}
-                        />
+                    <div className="sticky top-0 z-20 overflow-x-clip">
+                        <nav className="flex items-center h-16 px-4 bg-white shadow-sm gap-x-4 ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 md:px-6 lg:px-8">
+                            <div className="lg:hidden">
+                                <button
+                                    className="flex items-center justify-center rounded-lg transition duration-75 focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-70 -m-1.5 h-9 w-9 text-gray-400 hover:text-gray-500 focus-visible:ring-primary-600 dark:text-gray-500 dark:hover:text-gray-400 dark:focus-visible:ring-primary-500 fi-color-gray fi-topbar-close-sidebar-btn"
+                                    title="Collapse sidebar"
+                                    type="button"
+                                    onClick={() => {
+                                        setIsOpen(!isOpen);
+                                    }}
+                                >
+                                    <span className="sr-only">
+                                        Collapse sidebar
+                                    </span>
+                                    {isOpen ? closeIcon : openIcon}
+                                </button>
+                            </div>
+                            <div className="flex items-center ms-auto gap-x-4">
+                                {topbarEnd}
+                            </div>
+                        </nav>
                     </div>
-
-                    {/* Main content area */}
                     <main className="w-full h-full px-4 mx-auto md:px-6 lg:px-8 max-w-7xl">
                         {children}
                     </main>
                 </div>
-
-                {/* Sidebar overlay to close the sidebar on click */}
                 <div
                     onClick={() => setIsOpen(false)}
-                    className={`fi-sidebar-close-overlay fixed inset-0 z-30 bg-gray-950/50 transition duration-500 dark:bg-gray-950/75 lg:hidden ${
+                    className={`fixed inset-0 z-30 bg-gray-950/50 transition duration-500 dark:bg-gray-950/75 lg:hidden ${
                         isOpen ? "" : "hidden"
                     }`}
                     style={{
@@ -98,23 +69,36 @@ const BaseLayout = ({
                         display: isOpen ? "block" : "none",
                     }}
                 ></div>
-
-                {/* Sidebar */}
                 <aside
-                    className={`fi-sidebar fixed inset-y-0 start-0 z-30 flex flex-col h-screen content-start bg-white transition-all dark:bg-gray-900 lg:z-0 lg:bg-transparent lg:shadow-none lg:ring-0 lg:transition-none dark:lg:bg-transparent lg:translate-x-0 rtl:lg:-translate-x-0 fi-main-sidebar w-80 ${
+                    className={`fixed inset-y-0 start-0 z-30 flex flex-col h-screen content-start bg-white transition-all dark:bg-gray-900 lg:z-0 lg:bg-transparent lg:shadow-none lg:ring-0 lg:transition-none dark:lg:bg-transparent lg:translate-x-0 rtl:lg:-translate-x-0  
+                        ${isCollapsed ? "w-24" : "w-80"} ${
                         isOpen
-                            ? "fi-sidebar-open translate-x-0 shadow-xl ring-1 ring-gray-950/5 dark:ring-white/10 rtl:-translate-x-0 lg:sticky"
+                            ? " translate-x-0 shadow-xl ring-1 ring-gray-950/5 dark:ring-white/10 rtl:-translate-x-0 lg:sticky"
                             : "-translate-x-full rtl:translate-x-full lg:sticky"
                     }`}
                 >
                     <div className="overflow-x-clip">
-                        {/* Sidebar header */}
-                        <header className="flex items-center h-16 px-6 bg-white fi-sidebar-header ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 lg:shadow-sm">
-                            <div>{brand}</div>
+                        <header className="flex items-center h-16 px-6 bg-white justify-between ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 lg:shadow-sm group">
+                            <div>{isCollapsed ? iconBrand : brand}</div>
+                            <div className="flex justify-end text-gray-500 opacity-0 group-hover:opacity-100">
+                                {isCollapsed ? (
+                                    <button
+                                        className="hover:bg-gray-100 rounded bg-transparent p-1"
+                                        onClick={() => setIsCollapsed(false)}
+                                    >
+                                        <PanelLeftOpen className="w-5 h-5" />
+                                    </button>
+                                ) : (
+                                    <button
+                                        className="hover:bg-gray-100 rounded bg-transparent p-1"
+                                        onClick={() => setIsCollapsed(true)}
+                                    >
+                                        <PanelLeftClose className="w-5 h-5" />
+                                    </button>
+                                )}
+                            </div>
                         </header>
                     </div>
-
-                    {/* Sidebar navigation */}
                     <nav className="flex flex-col flex-grow px-6 py-8 overflow-x-hidden overflow-y-auto gap-y-7">
                         {sideMenu}
                     </nav>
@@ -124,4 +108,10 @@ const BaseLayout = ({
     );
 };
 
-export default BaseLayout;
+const BaseLayoutWithProvider = (props: PropsWithChildren<Props>) => (
+    <BaseLayoutProvider>
+        <BaseLayout {...props} />
+    </BaseLayoutProvider>
+);
+
+export default BaseLayoutWithProvider;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 /**
- * This file exports the `BaseLayout` component from the `base-layout.tsx` file.
- * The `base-layout.tsx` file contains the base layout component that renders a layout with a sidebar and a main content area.
- * This file is used to export the `BaseLayout` component for use in other parts of the application.
+ * This file exports the `BaseLayout` component and the `useBaseLayout` hook
+ * from the `base-layout` directory.
  *
  * @module components/index
- * @exports {React.ComponentType} BaseLayout - The `BaseLayout` component.
+ * @exports {React.ComponentType} BaseLayout - The `BaseLayout` component is the main layout component for the application.
+ *                                             It renders a layout with a sidebar and a main content area.
+ * @exports {Function} useBaseLayout - The `useBaseLayout` hook is a custom hook that provides access to the `isCollapsed` state
+ *                                    and the `setIsCollapsed` function from the `BaseLayoutProvider` context.
  */
 
 /**
@@ -15,3 +17,12 @@
  * @return {React.ReactElement} The rendered `BaseLayout` component.
  */
 export { default as BaseLayout } from "./base-layout";
+
+/**
+ * The `useBaseLayout` hook is a custom hook that provides access to the `isCollapsed` state
+ * and the `setIsCollapsed` function from the `BaseLayoutProvider` context.
+ *
+ * @function
+ * @return {{ isCollapsed: boolean, setIsCollapsed: (isCollapsed: boolean) => void }} An object containing the `isCollapsed` state and the `setIsCollapsed` function.
+ */
+export { useBaseLayout } from "./base-layout/base-layout-context";
